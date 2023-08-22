44c738885bffa6556e3092d3beca6a27ce9efbae
Pratik <pratikkaple1nov2002@gmail.com>
2023-08-22 20:02:20 +0530

    next day

--- Practice.cpp
+++ Practice.cpp
@@ -1,103 +1,55 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-class heap
+void heapify(vector<int> &arr, int n, int i)
 {
-private:
-    int arr[100];
-    int size;
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
 
-public:
-    heap()
+    if (left <= n && arr[largest] < arr[left])
     {
-        arr[0] = -1;
-        size = 0;
-    };
+        largest = left;
+    }
 
-    void insert(int val)
+    if (right <= n && arr[largest] < arr[right])
     {
-        size = size + 1;
-        int index = size;
-        arr[index] = val;
+        largest = right;
+    }
 
-        while (index > 1)
-        {
-            int parent = index / 2;
-
-            if (arr[parent] < arr[index])
-            {
-                swap(arr[index], arr[parent]);
-                index = parent;
-            }
-            else
-            {
-                return;
-            }
-        }
-    };
-
-    void print()
+    if (largest != i)
     {
-        for (int i = 1; i <= size; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    };
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
 
-    void deleteNodefromheap()
+void heapSort(vector<int> &arr, int n)
+{
+    int size = n;
+
+    while (size > 1)
     {
-        // base case
-        if (size == 0)
-        {
-            cout << "Nothing to delete" << endl;
-            return;
-        }
-
-        arr[1] = arr[size];
+        // step 1
+        swap(arr[size], arr[1]);
         size--;
 
-        // take root node to its correct position
-        int i = 1;
-        while (i < size)
-        {
-            int leftIndex = 2 * i;
-            int rightIndex = 2 * i + 1;
-
-            if (leftIndex < size && arr[i] < arr[leftIndex])
-            {
-                swap(arr[i], arr[leftIndex]);
-                i = leftIndex;
-            }
-            else if (rightIndex < size && arr[i] < arr[rightIndex])
-            {
-                swap(arr[i], arr[rightIndex]);
-                i = rightIndex;
-            }
-            else
-            {
-                return;
-            }
-        }
+        // step2
+        heapify(arr, size, 1);
     }
-};
+}
 
 int main(int argc, char const *argv[])
 {
-    heap h1;
+    vector<int> arr = {-1, 55, 52, 53, 51, 54};
 
-    h1.insert(50);
-    h1.insert(51);
-    h1.insert(53);
-    h1.insert(54);
-    h1.insert(56);
-    h1.print();
+    heapSort(arr, arr.size());
 
-    h1.deleteNodefromheap();
-    h1.print();
-
-    h1.deleteNodefromheap();
-    h1.print();
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
